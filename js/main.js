28c82cbf61459a1738ecf2496a44eff8e58a7d24
mikebkim <mikebkim@gmail.com>
2018-09-16 22:03:25 -0700

    adding nav underline

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Nav Targets
+
 $('a[href^="#"]').on('click', function (event) {
 
     var target = $(this.getAttribute('href'));
@@ -11,7 +13,7 @@ $('a[href^="#"]').on('click', function (event) {
 
 });
 
-// ===== Scroll to Top ==== 
+// Scroll to Top
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
@@ -24,3 +26,7 @@ $('#return-to-top').click(function () {      // When arrow is clicked
         scrollTop: 0                       // Scroll to top of body
     }, 500);
 });
+
+// Underline Nav
+
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
